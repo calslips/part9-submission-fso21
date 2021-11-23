f6dad447e97ac75c489f94e881b7f93de20f862e
Caleb Phillips <csphillips5@gmail.com>
2021-11-23 18:29:01 -0500

    9.1 create function calculateBmi, calculates BMI based on height(cm)/weight(kg) & returns message

---
+++ 9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,27 @@
+const bmiFormula = (cm: number, kg: number): number => {
+  return kg / (cm / 100) ** 2
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = bmiFormula(height, weight)
+
+  if (bmi < 16) {
+    return 'Underweight (Severe thinness)'
+  } else if (bmi >= 16 && bmi < 17) {
+    return 'Underweight (Moderate thinness)'
+  } else if (bmi >= 17 && bmi < 18.5) {
+    return 'Underweight (Mild thinness)'
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return 'Normal range (Healthy weight)'
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'Overweight (Pre-obese)'
+  } else if (bmi >= 30 && bmi < 35) {
+    return 'Obese (Class I)'
+  } else if (bmi >= 35 && bmi < 40) {
+    return 'Obese (Class II)'
+  } else {
+    return 'Obese (Class III)'
+  }
+}
+
+console.log(calculateBmi(180, 74))
